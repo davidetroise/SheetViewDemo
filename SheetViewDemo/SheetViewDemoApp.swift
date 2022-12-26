081b2b16e76abb2b8e8af6b012f10e2f29f49941
Davide Troise <davide@troise.net>
2022-12-26 18:08:33 +0100

    Initial Commit

---
+++ SheetViewDemo/SheetViewDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  SheetViewDemoApp.swift
+//  SheetViewDemo
+//
+//  Created by Davide Troise with Swift 5.0
+//  for the YouTube channel "Coding con Davide" https://bit.ly/3QJziJE
+//
+
+import SwiftUI
+
+@main
+struct SheetViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
