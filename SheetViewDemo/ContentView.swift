6651c05f7f900d4b2fce007009d270e839378573
Davide Troise <davide@troise.net>
2023-01-02 01:37:10 +0100

    Complete code

--- SheetViewDemo/ContentView.swift
+++ SheetViewDemo/ContentView.swift
@@ -8,13 +8,40 @@
 
 import SwiftUI
 
+struct SheetView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        Color.yellow
+            .ignoresSafeArea()
+            .overlay(alignment: .topTrailing) {
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "xmark.circle")
+                        .font(.largeTitle)
+                }
+                .padding()
+
+            }
+            .interactiveDismissDisabled()
+    }
+}
+
 struct ContentView: View {
+    @State private var isSheetShowing: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button {
+                isSheetShowing.toggle()
+            } label: {
+                Text("Show sheet view")
+            }
+            .sheet(isPresented: $isSheetShowing) {
+                SheetView()
+            }
+
         }
         .padding()
     }
